236eb0bdebaea5a6fc66c827f7591722541ef9ab
Karena Stevens <kaymark09@gmail.com>
2017-05-16 19:30:46 -0400

    Add Static Pages controller

---
+++ app/controllers/static_pages_controller.rb
@@ -0,0 +1,19 @@
+class StaticPagesController < ApplicationController
+  def home
+  end
+
+  def products
+  end
+
+  def about
+  end
+
+  def contact
+  end
+
+  def cart
+  end
+
+  def checkout
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,15 @@
 Rails.application.routes.draw do
+  get 'static_pages/home'
+
+  get 'static_pages/products'
+
+  get 'static_pages/about'
+
+  get 'static_pages/contact'
+
+  get 'static_pages/cart'
+
+  get 'static_pages/checkout'
+
   root 'application#hello'
 end
