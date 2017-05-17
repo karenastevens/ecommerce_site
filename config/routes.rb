c8bb2568395414b377a49e5a5875445651278112
Karena Stevens <kaymark09@gmail.com>
2017-05-17 15:05:57 -0400

    Finish layout and routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,10 @@
 Rails.application.routes.draw do
+  get 'users/new'
+
   root 'static_pages#home'
-
-  get 'static_pages/products'
-
-  get 'static_pages/about'
-
-  get 'static_pages/contact'
-
-  get 'static_pages/cart'
-
-  get 'static_pages/checkout'
+  get  '/products', to: 'static_pages#products'
+  get  '/about',    to: 'static_pages#about'
+  get  '/contact',  to: 'static_pages#contact'
+  get  '/cart',     to: 'static_pages#cart'
+  get  '/checkout', to: 'static_pages#checkout'
 end
--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -2,37 +2,37 @@ require 'test_helper'
 
 class StaticPagesControllerTest < ActionDispatch::IntegrationTest
   test "should get home" do
-    get root_url
+    get root_path
     assert_response :success
     assert_select "title", "Home | Starlight Kids"
   end
 
   test "should get products" do
-    get static_pages_products_url
+    get products_path
     assert_response :success
     assert_select "title", "Products | Starlight Kids"
   end
 
   test "should get about" do
-    get static_pages_about_url
+    get about_path
     assert_response :success
     assert_select "title", "About | Starlight Kids"
   end
 
   test "should get contact" do
-    get static_pages_contact_url
+    get contact_path
     assert_response :success
     assert_select "title", "Contact | Starlight Kids"
   end
 
   test "should get cart" do
-    get static_pages_cart_url
+    get cart_path
     assert_response :success
     assert_select "title", "Cart | Starlight Kids"
   end
 
   test "should get checkout" do
-    get static_pages_checkout_url
+    get checkout_path
     assert_response :success
     assert_select "title", "Checkout | Starlight Kids"
   end
